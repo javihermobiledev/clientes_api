240854dee041a0f88e30cf55704102835de36ca6
Javier Hernandez <javier.hernandez.hz@gmail.com>
2024-09-30 04:38:46 -0600

    Delete target/classes/scripts/insert-data.sql

--- target/classes/scripts/insert-data.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO cliente (nombre, email, telefono, direccion, fecha_nacimiento, fecha_registro, activo) VALUES
-('Javier Hernandez', 'javier.hernandez@example.com', '123-456-7890', 'Calle Falsa 123', '1985-10-15', '2024-09-16 12:00:00', true),
-('Isabel Silvestre', 'isabel.silvestre@example.com', '123-456-7891', 'Avenida Siempre Viva 742', '1990-05-22', '2024-09-16 12:00:00', true),
-('Alyssa Hernandez', 'alyssa.hernandez@example.com', '123-456-7892', 'Calle Mayor 456', '1995-08-10', '2024-09-16 12:00:00', true),
-('Isabella Hernandez', 'isabella.hernandez@example.com', '123-456-7893', 'Calle Secundaria 789', '2000-03-05', '2024-09-16 12:00:00', true);
